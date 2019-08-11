a314e5fbe7b7ba247a8e55af433f810ca68eab79
Don Denton <don@happycollision.com>
2019-08-11 02:33:38 -0500

    Don't include .DS_Store

--- src/routes/blog/_posts.js
+++ src/routes/blog/_posts.js
@@ -6,12 +6,14 @@ const dirname = "src/routes/blog/_posts";
 
 const posts = (() => {
   const md = new MarkdownIt({ html: true, typographer: true });
-  const dir = fs.readdirSync(dirname);
+  const dir = fs.readdirSync(dirname, "utf-8");
   const files = dir
+    .filter(fileName => !fileName.startsWith("."))
     .map(fileName => fs.readFileSync(`${dirname}/${fileName}`, "utf8"))
     .map(s => frontmatter(s))
     .map(f => ({ ...f.data, html: md.render(f.content) }));
 
+  console.log({ dir, files });
   return files;
 })();
 
