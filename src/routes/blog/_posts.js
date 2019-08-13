779bd6951fdbfd8e8d184bbb65170e1a78171e57
Don Denton <don@happycollision.com>
2019-08-12 21:15:15 -0500

    Allow svelte components in blog

--- src/routes/blog/_posts.js
+++ src/routes/blog/_posts.js
@@ -1,19 +1,39 @@
 import * as fs from "fs";
+import * as path from "path";
 import frontmatter from "frontmatter";
 import MarkdownIt from "markdown-it";
 
 const dirname = "src/routes/blog/_posts";
+const parentDir = path.join(dirname, "..");
+
+const STARTS_WITH_NUM = /^[0-9]/;
+const md = new MarkdownIt({ html: true, typographer: true });
 
 const posts = (() => {
-  const md = new MarkdownIt({ html: true, typographer: true });
-  const dir = fs.readdirSync(dirname, "utf-8");
-  const files = dir
+  const mdFileNames = fs.readdirSync(dirname, "utf-8");
+  const svelteFileNames = fs
+    .readdirSync(parentDir, "utf-8")
+    .filter(fileName => STARTS_WITH_NUM.test(fileName));
+  const files = mdFileNames
+    .concat(svelteFileNames)
+    .sort()
     .filter(fileName => !fileName.startsWith("."))
-    .map(fileName => fs.readFileSync(`${dirname}/${fileName}`, "utf8"))
-    .map(s => frontmatter(s))
-    .map(f => ({ ...f.data, html: md.render(f.content) }));
+    .map(fileName => {
+      const [basename, ext] = fileName.split(".");
+      if (ext === "md") {
+        const contents = fs.readFileSync(`${dirname}/${fileName}`, "utf8");
+        const fm = frontmatter(contents);
+        return { ...fm.data, slug: fileName, html: md.render(fm.content) };
+      } else {
+        const contents = fs.readFileSync(`${parentDir}/${fileName}`, "utf-8");
+        const [_full, _1, _2, title] = contents.match(
+          /^\s*export (const|let) title = ("|')(.*?)("|')/m,
+        );
+        return { slug: basename, title: title ? title : basename };
+      }
+    });
 
-  console.log({ dir, files });
+  console.log({ dir: mdFileNames, files });
   return files;
 })();
 
