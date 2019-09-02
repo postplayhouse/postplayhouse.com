ef324ec7209e79141ea4dda8883cee25c317f1ad
Don Denton <don@happycollision.com>
2019-09-02 00:33:09 -0500

    Transpile md and html files into svelte files

---
+++ src/data/_helpers/build-svelte-templates.ts
@@ -0,0 +1,22 @@
+// This script will build svelte files out of the html and md files in _posts
+// dir. Should be run as a pre-build step and also can be run with --clean to
+// remove the files it generated
+
+import * as fs from "fs"
+
+import { mdFiles } from "../../routes/news/_posts"
+
+mdFiles.forEach((details) => {
+  const filename = `src/routes/news/${details.slug}.svelte`
+  if (process.argv.includes("--clean")) {
+    if (fs.existsSync(filename)) {
+      fs.unlinkSync(filename)
+    }
+    return // don't rebuild if only cleaning
+  }
+  const exported = { title: details.title }
+  const script = `<script>${Object.keys(exported).map(
+    (k) => `\n  export let ${k} = "${exported[k].replace('"', '\\"')}"`,
+  )}\n</script>\n\n`
+  fs.writeFileSync(filename, `${script}${details.html}`, "utf8")
+})
--- src/routes/news/_posts.js
+++ src/routes/news/_posts.js
@@ -14,6 +14,13 @@ const md = new MarkdownIt({ html: true, typographer: true })
 const postsDirFiles = fs.readdirSync(postsDirname, "utf-8")
 const thisDirFiles = fs.readdirSync(thisDirname, "utf-8")
 
+/**
+ *
+ * @param {string} directory
+ * @param {...string} extensions
+ * @typedef {{basename: string, ext: string, contents: string, date: string}} Details
+ * @returns {(acc: Array<Details>, filename: string) => Array<Details>}
+ */
 const getDetails = (directory, ...extensions) => (acc, fileName) => {
   const [basename, ext] = fileName.split(".")
   if (extensions.indexOf(ext) === -1) {
@@ -23,7 +30,10 @@ const getDetails = (directory, ...extensions) => (acc, fileName) => {
   return [...acc, { basename, ext, contents, date: basename.slice(0, 10) }]
 }
 
-const mdFiles = postsDirFiles
+/**
+ * @type {Array<Details & {slug: string, html: string, title: string, [x: string]: string}>}
+ */
+export const mdFiles = postsDirFiles
   .reduce(getDetails(postsDirname, "md", "html"), [])
   .map(replaceJekyllTokens)
   .map((details) => {
@@ -33,7 +43,6 @@ const mdFiles = postsDirFiles
       ...fm.data,
       slug: details.basename,
       html: details.ext === "md" ? md.render(fm.content) : fm.content,
-      // html: fm.content,
     }
   })
 
@@ -41,20 +50,24 @@ const svelteFiles = thisDirFiles
   .reduce(getDetails(thisDirname, "svelte"), [])
   .filter((details) => STARTS_WITH_NUM.test(details.basename))
   .map((details) => {
-    const [_full, _1, _2, title] = details.contents.match(
-      /^\s*export (const|let) title = ("|')(.*?)("|')/m,
+    const match = details.contents.match(
+      /^\s*(export )?(const|let) title = ("|')(.*?)("|')/m,
     )
+    const title = match ? match[4] : details.basename
     // We only need the slug and title, because the actual svelte component
-    // will take over the content when it i routed.
+    // will take over the content when it is routed.
     return {
       ...details,
       slug: details.basename,
-      title: title ? title : details.basename,
+      title,
     }
   })
 
 const posts = svelteFiles
-  .concat(mdFiles)
+// .concat will add mdFiles, but filtered against the slugs of the svelte files.
+// If the transpile step happens, then all of the mdFiles will likely be
+// filtered out, since they will now exist as svelteFiles.
+  .concat(mdFiles.filter(mdFile => svelteFiles.map(s => s.slug).indexOf(mdFile.slug) === -1))
   .sort((a, b) => (a.slug > b.slug ? 1 : -1))
 
 export default posts
