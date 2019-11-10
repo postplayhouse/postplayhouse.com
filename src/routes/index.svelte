782b3c55677d92ace81609dcb2e8c9097713503a
Don Denton <don@happycollision.com>
2019-11-10 01:35:28 -0600

    CTA section of homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -21,12 +21,43 @@
   <Feature excerpt={true} />
 </div>
 
-<section
-  class="bg-green-200 p-3 cursor-pointer hover:bg-green-300"
-  on:click={toggleMailingList}>
-  <header class="text-xl">Join our mailing list</header>
-  <p>Stay informed about what’s happening at Post Playhouse</p>
-</section>
+<div class="md:flex flex-row-reverse items-stretch">
+  <section class=" mb-2">
+    <a href="/donations">
+      <img
+        alt="actors making a heart with thier hands"
+        src="/images/perennials/donations.jpg" />
+    </a>
+  </section>
+
+  <div class="md:flex flex-col md:mr-2">
+    <section
+      class="bg-green-200 p-3 cursor-pointer hover:bg-green-300 flex-initial
+      mb-2"
+      on:click={toggleMailingList}>
+      <header class="text-xl">Join our mailing list</header>
+      <p>Stay informed about what’s happening at Post Playhouse</p>
+    </section>
+
+    <section class="bg-green-200 p-3 flex-1 mb-2 flex flex-col">
+      <header class="text-xl">Stay connected with post</header>
+      <div class="flex flex-wrap justify-around items-center flex-1">
+        <a
+          class="block w-24 h-24 md:w-16 md:h-16 p-2 hover:bg-green-300"
+          href="http://facebook.com/post.playhouse">
+          <img src="/images/facebook-logo.svg" alt="Facebook logo" />
+        </a>
+        <a
+          class="block w-24 h-24 md:w-16 md:h-16 p-2 hover:bg-green-300"
+          href="http://twitter.com/postplayhouse">
+          <img src="/images/twitter-bird.svg" alt="Twitter logo" />
+        </a>
+      </div>
+
+    </section>
+  </div>
+
+</div>
 
 {#if showMailingList}
   <Modal on:close={toggleMailingList}>
