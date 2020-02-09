f12aba94d2dfb56311f80ff90ef09f552c2ee664
Don Denton <don@happycollision.com>
2020-02-08 20:53:41 -0600

    Errant console log, better unlinked page strategy

--- src/routes/data/preload.js
+++ src/routes/data/preload.js
@@ -2,7 +2,6 @@ export function preload(_routeInfo) {
   return this.fetch(`data.json`)
     .then((r) => r.json())
     .then((data) => {
-      console.log("DATATATATAATA", data)
       return { site: data }
     })
 }
---
+++ src/routes/entries.svelte
@@ -0,0 +1,15 @@
+<script>
+  const yearsWithPages = [2019, 2020]
+
+  const routes = ["/", "/bio-submission"]
+    .concat(yearsWithPages.map((y) => `/productions/${y}`))
+    .concat(yearsWithPages.map((y) => `/who/${y}`))
+</script>
+
+<ul>
+  {#each routes as route}
+    <li>
+      <a href={route}>{route}</a>
+    </li>
+  {/each}
+</ul>
