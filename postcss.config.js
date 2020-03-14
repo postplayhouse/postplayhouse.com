a2152d08cb936a5cc408db502656034428ab12fd
Don Denton <don@happycollision.com>
2020-03-13 22:47:43 -0500

    Make css build cleaner

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,5 @@
+const prod = process.env.NODE_ENV === "production"
+
 const tailwindcss = require("tailwindcss")
 
 const purgecss = require("@fullhuman/postcss-purgecss")({
@@ -5,10 +7,13 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
   defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 })
 
+const cssNano = require("cssnano")({ preset: "default" })
+
 module.exports = {
   plugins: [
     tailwindcss("./tailwind.js"),
-
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
-  ],
+    require("autoprefixer"),
+    prod && purgecss,
+    prod && cssNano,
+  ].filter(Boolean),
 }
