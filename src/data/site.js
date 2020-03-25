c82881a1f29e2d0b7f20a90a6d66dd8a7827a4c6
Don Denton <don@happycollision.com>
2020-03-25 02:26:00 -0500

    Advance season

--- src/data/site.js
+++ src/data/site.js
@@ -14,7 +14,7 @@ export default {
   twitter: "postplayhouse",
   facebook: "post.playhouse",
   castingComplete: prod ? castingComplete : true,
-  season: 2020,
+  season: 2021,
   description:
     "Post Playhouse, Inc. is Northwestern Nebraska's favorite live theatre company, producing several musicals running in a repertory schedule every summer by bringing together highly skilled creative professionals from across the country and nearby. Post Playhouse, Inc. performs its productions at the theatre in Fort Robinson State Park.",
   boxOfficePhone: "1-888-665-1976",
