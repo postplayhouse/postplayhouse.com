d7f6b7f8f5f170cb4ec0421fd97fb475a63a4e6b
Don Denton <don@happycollision.com>
2020-03-25 02:26:32 -0500

    Add message to who 2020

--- src/routes/who/[slug].svelte
+++ src/routes/who/[slug].svelte
@@ -31,6 +31,17 @@
 </script>
 
 {#each ['rest', 'Board', 'Additional'] as groupName}
+
+  {#if slug === '2020'}
+    <p>
+      <a href="/news/2020-03-25-season-cancelled" class="link-green">
+        Our 2020 Season was sadly cancelled.
+      </a>
+      Preserved here are the people we were really looking forward to working
+      with.
+    </p>
+  {/if}
+
   {#if groupedPeople[groupName].length}
     <h2 class="h2 sm:sticky top-0 bg-white mb-4">
       {groupName === 'rest' ? generalGroupName : groupName}
