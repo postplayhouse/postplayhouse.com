f3f0f922bb8f48487521679959f41991846b7ec1
Don Denton <don@happycollision.com>
2020-03-25 10:31:04 -0500

    check md files for styles

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ const prod = process.env.NODE_ENV === "production"
 const tailwindcss = require("tailwindcss")
 
 const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./src/**/*.svelte", "./src/**/*.html"],
+  content: ["./src/**/*.svelte", "./src/**/*.md", "./src/**/*.html"],
   defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 })
 
