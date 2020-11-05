dba3cd4b459899b89cce13b7399876f91714ccb5
Don Denton <don@happycollision.com>
2020-11-04 21:05:23 -0500

    Update messaging on donation form
    I've removed the custom header on the Eleo form and just put it here
    instead. Better to control it on our site that through Eleo anyhow.

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -3,12 +3,10 @@
   import Header from "../components/Header.svelte"
   import Footer from "../components/Footer.svelte"
   import PersistentModal from "../components/PersistentModal.svelte"
-  import { showDonateModal } from '../data/stores'
+  import { showDonateModal } from "../data/stores"
 
   export let segment
 </script>
-
-
 
 <style>
   main {
@@ -20,7 +18,17 @@
 
   <h2 class="h2">Donate Now!</h2>
 
-  <iframe title="Donate to Post Playhouse" name='ELEOForm' id='ELEOForm' style='width:100%;min-width:320px;max-width:900px;min-height:1000px;border-width:0px;border-style:none;' scrolling='no' src='https://www.eleoonline.net/Pages/WebForms/Mobile/ShowFormMobile.aspx?id=f790c257-b67f-4508-9ff9-0fc3a16f04d2&linkto=670' ></iframe>
+  <h3 class="h3">
+    Help make our next season happen by donating to our Annual Fund!
+  </h3>
+
+  <iframe
+    title="Donate to Post Playhouse"
+    name="ELEOForm"
+    id="ELEOForm"
+    style="width:100%;min-width:320px;max-width:900px;min-height:1000px;border-width:0px;border-style:none;"
+    scrolling="no"
+    src="https://www.eleoonline.net/Pages/WebForms/Mobile/ShowFormMobile.aspx?id=f790c257-b67f-4508-9ff9-0fc3a16f04d2&linkto=670" />
 </PersistentModal>
 
 <Header />
