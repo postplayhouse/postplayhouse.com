0dc3077ce40c7c8edac126745e72eec88a107b3e
Don Denton <don@happycollision.com>
2021-02-11 01:31:45 -0500

    Add more programatic industry professionals page

--- src/helpers/blog-dir-metadata.js
+++ src/helpers/blog-dir-metadata.js
@@ -7,7 +7,7 @@ const STARTS_WITH_DATE = /^\d{4}-\d{2}-\d{2}-/
  *
  * @param {string} directory
  * @param {string[]} extensions
- * @typedef {{basename: string, ext: string, contents: string, date: string}} Details
+ * @typedef {{basename: string, ext: string, content: string, date: string}} Details
  * @returns {(acc: Array<Details>, filename: string) => Array<Details>}
  */
 const getDetails = (directory, extensions) => (acc, fileName) => {
@@ -15,8 +15,8 @@ const getDetails = (directory, extensions) => (acc, fileName) => {
   if (extensions.indexOf(ext) === -1) {
     return acc
   }
-  const contents = fs.readFileSync(`${directory}/${fileName}`, "utf8")
-  return [...acc, { basename, ext, contents, date: basename.slice(0, 10) }]
+  const content = fs.readFileSync(`${directory}/${fileName}`, "utf8")
+  return [...acc, { basename, ext, content, date: basename.slice(0, 10) }]
 }
 
 /**
@@ -62,17 +62,18 @@ const prepFiles = (extensions, mapFn = (x) => x) => (dirPath, fileNames) =>
   fileNames.reduce(getDetails(dirPath, extensions), []).map(mapFn)
 
 const prepMdsvexFiles = prepFiles(["md"], (details) => {
-  const fm = frontmatter(details.contents).data
+  const fm = frontmatter(details.content)
   return {
     ...details,
-    ...fm,
+    ...fm.data,
+    content: fm.content,
     slug: details.basename,
-    title: fm.title || titleFromBasename(details.basename),
+    title: fm.data.title || titleFromBasename(details.basename),
   }
 })
 
 const prepSvelteFiles = prepFiles(["svelte"], (details) => {
-  const match = details.contents.match(
+  const match = details.content.match(
     /^\s*(export )?(const|let) title = ("|')(.*?)("|')/m,
   )
   // We only need the slug and title, because the actual svelte component
@@ -83,6 +84,12 @@ const prepSvelteFiles = prepFiles(["svelte"], (details) => {
     title: match ? match[4] : titleFromBasename(details.basename),
   }
 })
+
+function loadLegitFiles(dirPath) {
+  return fs
+    .readdirSync(dirPath, "utf-8")
+    .filter((fileName) => STARTS_WITH_DATE.test(fileName))
+}
 
 /**
  * This will return an array of articles' metadata. Frontmatter data in md files
@@ -97,9 +104,7 @@ const prepSvelteFiles = prepFiles(["svelte"], (details) => {
  *
  */
 export function postsMetadata(dirPath) {
-  const thisDirBlogFiles = fs
-    .readdirSync(dirPath, "utf-8")
-    .filter((fileName) => STARTS_WITH_DATE.test(fileName))
+  const thisDirBlogFiles = loadLegitFiles(dirPath)
 
   return prepSvelteFiles(dirPath, thisDirBlogFiles)
     .concat(prepMdsvexFiles(dirPath, thisDirBlogFiles))
---
+++ src/routes/industry-professionals/2020-03-25-season-cancelled.md
@@ -0,0 +1,9 @@
+---
+title: Summer 2020 cancelled, stay tuned
+active: false
+---
+
+[Our 2020 Season has been cancelled.](/news/2020-03-25-season-cancelled)
+
+We will let you know about job opportunities and auditions for our 2021 Summer
+Season as soon as we can!
