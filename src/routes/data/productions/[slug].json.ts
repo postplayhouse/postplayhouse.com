efccf3438a42a5be5234ac45493c217f07a3ff34
Don Denton <don@happycollision.com>
2021-09-18 11:41:38 -0400

    WIP data entered

--- src/routes/data/productions/[slug].json.ts
+++ src/routes/data/productions/[slug].json.ts
@@ -2,6 +2,14 @@ import site from "../../../data/site"
 import data from "../../../data/_yaml"
 import type { RequestHandler } from "@sveltejs/kit"
 
-export const get: RequestHandler = (req) => ({
-  body: { site, productions: data.productions[req.params["slug"]] },
-})
+export const get: RequestHandler = (req) => {
+  // console.log(req.params["slug"])
+  // console.log(typeof req.params["slug"])
+  // console.log(data.productions[req.params["slug"]])
+  return {
+    body: {
+      site,
+      productions: data.productions[req.params["slug"]] || [],
+    },
+  }
+}
