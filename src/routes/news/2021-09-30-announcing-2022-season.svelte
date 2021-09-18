019dca8457597107d08c8477395269624ae66e99
Don Denton <don@happycollision.com>
2021-09-18 14:53:58 -0400

    Announcement post

---
+++ src/routes/news/2021-09-30-announcing-2022-season.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import site from "../../data/site"
+  export const title = "Announcing our 2022 season!"
+</script>
+
+<!-- <div class="bg-green-100 p-4 text-center">
+  <strong>Update:</strong>
+  Individual tickets are now on sale, as well as Season Subscriptions!
+</div> -->
+
+<div class="w-2/3 m-auto">
+  <img src="/images/2022/the-sound-of-music.jpg" alt="" />
+</div>
+<div class="flex gap-2">
+  <div class="w-1/2">
+    <img src="/images/2022/damn-yankees.png" alt="" />
+  </div>
+  <div class="w-1/2">
+    <img src="/images/2022/desperate-measures.jpg" alt="" />
+  </div>
+</div>
+<div class="flex gap-2">
+  <div class="w-1/2">
+    <img src="/images/2022/something-rotten.jpg" alt="" />
+  </div>
+  <div class="w-1/2">
+    <img src="/images/2022/church-basement-ladies.png" alt="" />
+  </div>
+</div>
+
+<p class="text-center m-4">Tickets will be on sale soon!</p>
+
+<!-- <div class="flex justify-center">
+  <a href={site.ticketsLink} class="btn-p">
+    Get Your Tickets Now
+  </a>
+</div> -->
