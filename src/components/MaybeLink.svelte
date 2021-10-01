ac3168feec63955350396c2051fc812b19ed371f
Don Denton <don@happycollision.com>
2021-10-01 17:03:57 -0400

    Add possible links to sponsors

---
+++ src/components/MaybeLink.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let href: string | undefined = undefined
+  let className = ""
+  export { className as class }
+</script>
+
+{#if href}
+  <a href="{href}" class="{className}"><slot /></a>
+{:else}
+  <span href="{href}" class="{className}"><slot /></span>
+{/if}
