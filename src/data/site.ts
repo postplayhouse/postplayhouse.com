924d45060d962f421084ddddbaca253c4631cd36
Don Denton <don@happycollision.com>
2021-11-01 09:51:57 -0400

    Make tickets available

--- src/data/site.ts
+++ src/data/site.ts
@@ -4,7 +4,7 @@ const liveUrl = "https://postplayhouse.com"
 
 // Actual values. If dev, these may change below.
 const castingComplete = false
-const ticketsAvailable = false
+const ticketsAvailable = true
 
 // When doing a Netlify PR Deploy, use the provided url
 const url = live
