d6593f876c01aac1eb6959b51a7231ffd35662f3
Don Denton <don@happycollision.com>
2022-02-14 09:49:28 -0500

    Add info page for ad buys

---
+++ src/routes/program-ad-info.svelte
@@ -0,0 +1,100 @@
+<script lang="ts">
+  import Markdown from "$components/Markdown.svelte"
+</script>
+
+<svelte:head>
+  <title>Post Playhouse: Program Ad Information</title>
+</svelte:head>
+
+<Markdown
+  source="{`
+# Program Ad Information
+
+Thank you so much for supporting Post Playhouse via an ad space purchase in our
+program! This page is here to give you information about how to get the most out
+of your ad.
+
+First, you should know that the person you'll want to send your materials to is
+Don Denton. You can reach him at <don@postplayhouse.com>. He may not yet know
+who you are, so put "Program Ad: YOUR BUSINESS NAME" in the subject line. Read
+on to know what you should send his way.
+
+## Terminology
+
+We will refer to the following terminology in the rest of this article:
+
+- **Artwork**: any image file such as a SVG, Adobe Photoshop/Illustrator file,
+  JPEG, PNG, or (least preferred) a PDF
+- **DPI (or PPI)**: Dots per inch or pixels per inch. This is a measure of the
+  **resolution** of your artwork.
+- **Copy**: the text that you'd like to see in your ad. Things like "Visit us!",
+  or "Mention Post Playhouse for a $3 discount"
+
+## Dimensions
+
+Your ad is purchased at one of four sizes: Full Page, Half Page, 1/4 Page, or
+1/8 Page. The dimensions are explained in the following image:
+
+![chart of ad dimensions](/g/images/perennials/ad-dimensions.png)
+
+**Please ensure that the artwork you send is close to 300ppi or more.**
+
+For example, if you purchased a 1/8 Page ad and you have a logo that is intended
+to span the full width of the ad spot, you'll want to make sure your logo image
+is around (3.9 inches * 300 ppi) 1,170 pixels wide.
+
+If your image isn't that big and you have no way of getting a better one from
+your designer, don't enlarge it. Just send it to Don as is and let him know that
+is the best you've got. Don will make it look as good as possible!
+
+## What we need from you
+
+You are purchasing a space for your ad to run, and we want to be sure that you
+understand that you are NOT purchasing any kind of design services. So if you
+have no idea  what you'd like to do in the space you've provided, we can't
+help you too much in that regard. (Don would actually _love_ to help you design
+your ad, but he doesn't have the time to devote to it.)
+
+However, if you have at least _some_ idea of what you'd like to see, Don can
+absolutely work with you. Here's the breakdown of what materials we need from
+you, based on your level of confidence in your vision for your ad.
+
+### You have some idea what you'd like to see
+
+If you are a little unsure of what you'd like to see in your ad, but you have a
+few ideas, we can work with you. The one thing we cannot do is create images for
+you.
+
+So just email Don and let him know the few ideas you have about what copy you'd
+like to see and the few images you can't decide between. He will choose the best
+of the bunch for the ad space size you've purchased and show you the result.
+
+Even if you only have some copy, and your business's logo as your artwork,
+that's fine!
+
+### You know exactly what you want
+
+Have your in-house designer create an ad that fits the dimensions you've
+purchased, making sure to bake all your ad copy into the image itself. Export
+the image at 300ppi and send it along to Don.
+
+## Additional, helpful materials to send us
+
+- If you have a website, please include the address in your email to Don. He can
+use it to get a quick feel for your branding.
+
+- If you've run **other ads** in the past, you can send images of those ads as
+examples as well.
+
+- If you have an **in-house designer**, or just the **contact information of the
+person that designed your logo**, you can send that as well. Don may not
+actually reach out to that person, but if he needs a better resolution of your
+logo, thier contact information can save several steps.
+
+- If you have the **original logo design files** for your company's logo, please
+send those as well. In the last 12 years, Don has recieved about 4 logos that
+were a large enough resolution that he didn't have to go back and ask for a
+better/bigger one. Most of the time, someone at the company could locate a
+better file.
+`}"
+/>
