5d9eb77b6b8582d3d9a0599562e17d8f0dd7fa74
Don Denton <don@happycollision.com>
2022-03-14 22:10:14 -0400

    Fix .md types

--- src/imports.d.ts
+++ src/imports.d.ts
@@ -1,6 +1,3 @@
-import type { SvelteComponent } from "svelte"
-
 declare module "*.md" {
-  const Component: SvelteComponent
-  export default Component
+  export { SvelteComponent as default } from "svelte"
 }
