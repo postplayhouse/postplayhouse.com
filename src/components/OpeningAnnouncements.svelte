577ac6b84a7b2202f582549c01fec04ab583c966
Don Denton <don@happycollision.com>
2022-08-13 22:53:26 -0400

    Update closing day messages

--- src/components/OpeningAnnouncements.svelte
+++ src/components/OpeningAnnouncements.svelte
@@ -19,6 +19,9 @@
 
   const daysToClosing = diffDays(today, getDateFor(closingDate))
   const isBeforeClosing = daysToClosing >= 0
+  const isAfterClosing =
+    // 4pm MDT
+    new Date() > new Date(`${closingDate}T16:00:00.000-06:00`)
 
   const enhancedProductions = productions
     .map((p) => ({
@@ -48,15 +51,51 @@
     isBeforeClosing && !enhancedProductions.find((p) => p.daysUntilOpening > 0)
 </script>
 
-{#if !isBeforeClosing}
+{#if isAfterClosing}
   <h3 class="h1 my-8">Thank you for a wonderful season!</h3>
 
   <p class="text-3xl my-8">
-    We were delighted to bring you a summer season full of entertainment, and we
-    can't wait to see you next year!
+    We were delighted to bring you a summer season full of entertainment!
   </p>
 
-  <slot name="seasonArtworkImage" />
+  <h3 class="h1">
+    A Special Message from <span class="whitespace-nowrap">Tom Ossowski</span>
+  </h3>
+
+  <div class="my-4 space-y-4 text-lg">
+    <p>Dear Friends,</p>
+
+    <p>
+      Thank you all for an amazing post-Covid come back 2022 season. I have been
+      Producing Artistic Director for the past 16 years and I’m proud of the
+      work we have done at the Post Playhouse presenting theatre in the
+      Panhandle of Nebraska. It has always been my goal to harness the power of
+      live theatre to bring people in our communities together and I am so
+      pleased to have successfully led Post Playhouse in that mission all these
+      years. I cherish the many friendships here in Northwest Nebraska and have
+      been very moved by the magic we have made together on the Post Playhouse
+      stage.
+    </p>
+
+    <p>
+      It is with mixed emotions that I am moving on after this summer.
+      Everything in life has a season and this has been a wonderful season of my
+      life. However, due to shifting long-term strategy planning within the
+      organization, I have decided to hand off the reins to a new Artistic
+      Director to usher Post Playhouse into the future. I wish you all the best
+      and thank you for your kind support and words of encouragement. I am
+      looking forward to a little vacation and then finding a new place that
+      needs my artistic directorship. I hope I’ll be able to bring some of the
+      magic we created at the Post Playhouse along with me. Thank you for all
+      the memories.
+    </p>
+
+    <p>
+      Sincerely,<br />
+
+      Tom Ossowski
+    </p>
+  </div>
 {:else}
   {#if openingSoon}
     <h3 class="h1 font-uber">
@@ -104,8 +143,9 @@
         </p>
       {/if}
 
-      <div class="flex justify-center m-4">
-        <TicketsButton />
+      <div class="m-4 h3">
+        (We are sold out! But you can call the box office to inquire about wait
+        lists.)
       </div>
     </div>
   {/if}
