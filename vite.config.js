22773adc636bae2b9c927753386f41249675d4cb
Don Denton <don@happycollision.com>
2022-09-18 22:58:14 -0400

    upgraded, but broken
    When querying productions?

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import path from "path"
+import { sveltekit } from "@sveltejs/kit/vite"
+import WatchAndRun from "@kitql/vite-plugin-watch-and-run"
+
+import replace from "@rollup/plugin-replace"
+import { replacements } from "./svelte.config"
+
+/** @type {import('vite').UserConfig} */
+const config = {
+  plugins: [
+    replace({
+      values: replacements.reduce(
+        (acc, [key, value]) => ({ ...acc, [key]: value }),
+        {},
+      ),
+      preventAssignment: true,
+    }),
+    WatchAndRun([
+      {
+        watch: "**/src/data/**/*.yml",
+        run: "touch ./src/data/_yaml.ts",
+      },
+    ]),
+    sveltekit(),
+  ],
+  resolve: {
+    alias: {
+      $components: path.resolve("./src/components"),
+    },
+  },
+}
+
+export default config
