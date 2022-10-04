179ce28e3e7dadae0049444870fc43988e10282e
Don Denton <don@happycollision.com>
2022-10-03 22:59:49 -0400

    Bump many

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import path from "path"
 import { sveltekit } from "@sveltejs/kit/vite"
-import WatchAndRun from "@kitql/vite-plugin-watch-and-run"
+import WatchAndRun from "vite-plugin-watch-and-run"
 
 import replace from "@rollup/plugin-replace"
 import { replacements } from "./svelte.config"
