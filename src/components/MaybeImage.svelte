df6a317666574955a615295b0364308fb2097e9d
Don Denton <don@happycollision.com>
2023-04-07 21:44:37 -0400

    Refactor MaybeImage to only download needed img

--- src/components/MaybeImage.svelte
+++ src/components/MaybeImage.svelte
@@ -8,28 +8,31 @@
     src = [src]
   }
 
-  function checkImage(imageSrc: string, good: Callback, bad = () => {}) {
-    var img = new Image()
-    img.onload = good
-    img.onerror = bad
-    img.src = imageSrc
-  }
-
-  function reportSrcExists(srcIndex: number) {
-    winningSrcIndex = srcIndex < winningSrcIndex ? srcIndex : winningSrcIndex
-    winningSrc = src[winningSrcIndex]
+  function checkImageExists(imageSrc: string) {
+    return new Promise((res) => {
+      var img = new Image()
+      img.onload = () => res(true)
+      img.onerror = () => res(false)
+      img.src = imageSrc
+    })
   }
 
   // The lowest index for src wins
   $: winningSrc = undefined as string | undefined
-  let winningSrcIndex = Infinity
 
   // We render nothing unless mounted in the client. SSR will result in lots of
   // thrown error noise
-  let mounted = false
-  onMount(() => {
-    mounted = true
-    src.forEach((path, i) => checkImage(path, () => reportSrcExists(i)))
+  onMount(async () => {
+    for (const path of src) {
+      if (await checkImageExists(path)) {
+        winningSrc = path
+        break
+      }
+    }
+
+    if (!winningSrc) {
+      console.warn(`No image found for ${src.join()}`)
+    }
   })
 </script>
 
