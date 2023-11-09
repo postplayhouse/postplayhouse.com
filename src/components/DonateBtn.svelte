1cb1f9de1da5b62290b8789d902860c2af70b706
Don Denton <don@happycollision.com>
2023-11-09 09:42:45 -0500

    Drop extra click to Eleo

--- src/components/DonateBtn.svelte
+++ src/components/DonateBtn.svelte
@@ -1,10 +1,8 @@
-<script lang="ts">
-  import { showDonateModal } from "$data/stores"
-
-  let className = "btn btn-p block mx-auto my-8"
-  export { className as class }
-</script>
-
-<button class="{className}" on:click="{showDonateModal.toggle}"
-  ><slot>Donate Online Now</slot></button
->
+<div class="flex justify-center items-center my-8">
+  <a
+    href="https://www.eleoonline.net/Pages/WebForms/Mobile/ShowFormMobile.aspx?id=f790c257-b67f-4508-9ff9-0fc3a16f04d2&linkto=670"
+    target="_blank"
+    rel="noopener noreferrer"
+    class="btn btn-p inline-block"><slot>Donate Online Now</slot></a
+  >
+</div>
--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -3,32 +3,7 @@
   import Nav from "$components/Nav.svelte"
   import Header from "$components/Header.svelte"
   import Footer from "$components/Footer.svelte"
-  import PersistentModal from "$components/Modal/PersistentModal.svelte"
-  import { showDonateModal } from "$data/stores"
 </script>
-
-<PersistentModal on:close="{showDonateModal.toggle}" show="{$showDonateModal}">
-  <h2 class="h2">Donate Now!</h2>
-
-  <h3 class="h3">
-    Help make our next season happen by donating to our Annual Fund!
-  </h3>
-  <p>
-    Clicking the link below will take you to a site called Eleo Online, where
-    you can complete your donation. Thank you so much for your support!
-  </p>
-
-  <div class="text-center mt-4">
-    <a
-      href="https://www.eleoonline.net/Pages/WebForms/Mobile/ShowFormMobile.aspx?id=f790c257-b67f-4508-9ff9-0fc3a16f04d2&linkto=670"
-      target="_blank"
-      rel="noopener noreferrer"
-      class="btn p-4 px-6 inline-block"
-    >
-      Visit our donation form</a
-    >
-  </div>
-</PersistentModal>
 
 <Header />
 <Nav activePath="{$page.url.pathname}" />
