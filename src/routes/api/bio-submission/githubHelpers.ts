d1a2254a4e13c593ee31d0a3a0cb35518d7dd0a9
Don Denton <don@happycollision.com>
2024-02-04 22:11:49 -0500

    refactor

--- src/routes/api/bio-submission/githubHelpers.ts
+++ src/routes/api/bio-submission/githubHelpers.ts
@@ -5,6 +5,137 @@ const octokit = new Octokit({
 	auth: env["GITHUB_ACCESS_TOKEN"],
 })
 
+async function getOrCreateBranch({
+	owner,
+	repo,
+	baseBranch,
+	targetBranch,
+}: {
+	owner: string
+	repo: string
+	baseBranch: string
+	targetBranch: string
+}) {
+	try {
+		return await octokit.git.getRef({
+			owner,
+			repo,
+			ref: `heads/${targetBranch}`,
+		})
+	} catch {
+		const baseSha = await octokit.git
+			.getRef({
+				owner,
+				repo,
+				ref: `heads/${baseBranch}`,
+			})
+			.then((res) => res.data.object.sha)
+		return await octokit.git.createRef({
+			owner,
+			repo,
+			ref: `refs/heads/${targetBranch}`,
+			sha: baseSha,
+		})
+	}
+}
+
+async function getOrCreatePr({
+	owner,
+	repo,
+	baseBranch,
+	prBranch,
+	prMessage,
+}: {
+	owner: string
+	repo: string
+	baseBranch: string
+	prBranch: string
+	prMessage: string
+}) {
+	const url = await octokit.pulls
+		.list({
+			owner,
+			repo,
+			head: `${owner}:${prBranch}`,
+			base: baseBranch,
+			state: "open",
+		})
+		.then((res) => res.data[0]?.html_url)
+
+	if (url) return url
+
+	return octokit.pulls
+		.create({
+			owner,
+			repo,
+			title: prMessage,
+			head: prBranch,
+			base: baseBranch,
+		})
+		.then((res) => res.data.html_url)
+}
+
+async function commitNewFileContent({
+	owner,
+	repo,
+	filePath,
+	commitMessage,
+	newContent,
+	branchName,
+	branchSha,
+}: {
+	owner: string
+	repo: string
+	filePath: string
+	commitMessage: string
+	newContent: (
+		prev: string,
+	) => { success: true; newContent: string } | { success: false; error: string }
+	branchName: string
+	branchSha: string
+}) {
+	const currentContentResponse = await octokit.repos.getContent({
+		owner,
+		repo,
+		path: filePath,
+		ref: branchSha,
+	})
+
+	if (currentContentResponse.status !== 200) {
+		return { success: false, error: `File not found at ${filePath}` }
+	}
+
+	if (
+		Array.isArray(currentContentResponse.data) ||
+		currentContentResponse.data.type !== "file"
+	) {
+		return { success: false, error: `No file at ${filePath}` }
+	}
+
+	const currentContent = Buffer.from(
+		currentContentResponse.data.content,
+		"base64",
+	).toString("utf-8")
+
+	const newContentResult = newContent(currentContent)
+
+	if (!newContentResult.success) {
+		return { success: false, error: newContentResult.error }
+	}
+
+	const blobSha = currentContentResponse.data.sha
+
+	await octokit.repos.createOrUpdateFileContents({
+		owner,
+		repo,
+		path: filePath,
+		message: commitMessage,
+		content: Buffer.from(newContentResult.newContent).toString("base64"),
+		branch: branchName,
+		sha: blobSha,
+	})
+}
+
 export async function updateAndPr({
 	owner,
 	repo,
@@ -25,97 +156,30 @@ export async function updateAndPr({
 	prBranch: string
 }) {
 	try {
-		const masterRef = await octokit.git.getRef({
+		const prBranchSha = await getOrCreateBranch({
 			owner,
 			repo,
-			ref: `heads/${baseBranch}`,
-		})
+			baseBranch,
+			targetBranch: prBranch,
+		}).then((res) => res.data.object.sha)
 
-		const masterSha = masterRef.data.object.sha
-
-		const markerBranchRef = await octokit.git
-			.getRef({
-				owner,
-				repo,
-				ref: `heads/${prBranch}`,
-			})
-			.catch(() => undefined)
-
-		const markerBranchSha = markerBranchRef?.data.object.sha
-
-		const startingSha = markerBranchSha || masterSha
-
-		const currentContentResponse = await octokit.repos.getContent({
+		await commitNewFileContent({
 			owner,
 			repo,
-			path: filePath,
-			ref: startingSha,
+			filePath,
+			commitMessage: commitAndPrMessage,
+			newContent,
+			branchName: prBranch,
+			branchSha: prBranchSha,
 		})
 
-		if (currentContentResponse.status !== 200) {
-			return { success: false, error: `File not found at ${filePath}` }
-		}
-
-		if (
-			Array.isArray(currentContentResponse.data) ||
-			currentContentResponse.data.type !== "file"
-		) {
-			return { success: false, error: `No file at ${filePath}` }
-		}
-
-		const currentContent = Buffer.from(
-			currentContentResponse.data.content,
-			"base64",
-		).toString("utf-8")
-
-		const newContentResult = newContent(currentContent)
-
-		if (!newContentResult.success) {
-			return { success: false, error: newContentResult.error }
-		}
-
-		if (!markerBranchSha) {
-			await octokit.git.createRef({
-				owner,
-				repo,
-				ref: `refs/heads/${prBranch}`,
-				sha: masterSha,
-			})
-		}
-
-		const blobSha = currentContentResponse.data.sha
-
-		await octokit.repos.createOrUpdateFileContents({
+		const pullRequest = await getOrCreatePr({
 			owner,
 			repo,
-			path: filePath,
-			message: commitAndPrMessage,
-			content: Buffer.from(newContentResult.newContent).toString("base64"),
-			branch: prBranch,
-			sha: blobSha,
+			baseBranch,
+			prBranch,
+			prMessage: commitAndPrMessage,
 		})
-
-		let pullRequest: string | undefined = (
-			await octokit.pulls.list({
-				owner,
-				repo,
-				head: `${owner}:${prBranch}`,
-				base: baseBranch,
-				state: "open",
-			})
-		).data[0]?.html_url
-
-		if (!pullRequest) {
-			pullRequest = (
-				await octokit.pulls.create({
-					owner,
-					repo,
-					title: commitAndPrMessage,
-					head: prBranch,
-					base: baseBranch,
-				})
-			).data.html_url
-		}
 
 		return { success: true, pullRequest } as const
 	} catch (error) {
