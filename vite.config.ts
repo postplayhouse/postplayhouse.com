fd6c227353547cec3e3403f74caa48e9fc4dd9a9
Don Denton <don@happycollision.com>
2024-04-14 00:22:03 -0400

    sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { sveltekit } from "@sveltejs/kit/vite"
 import { defineConfig } from "vitest/config"
 import { replace } from "./replacements.config"
+import { sentrySvelteKit } from "@sentry/sveltekit"
 
 export default defineConfig({
-	plugins: [replace(), sveltekit()],
+	plugins: [
+		sentrySvelteKit({
+			sourceMapsUploadOptions: {
+				org: "post-playhouse",
+				project: "javascript-sveltekit",
+			},
+		}),
+		replace(),
+		sveltekit(),
+	],
 	test: {
 		include: ["src/**/*.{test,spec}.{js,ts}"],
 	},
