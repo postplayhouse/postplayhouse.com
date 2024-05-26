998eae167f554b9f9ca7987fbed8cda740101808
Don Denton <don@happycollision.com>
2024-05-25 23:57:17 -0400

    Fix bad anchors

--- src/routes/(app)/who/[year]/+page.svelte
+++ src/routes/(app)/who/[year]/+page.svelte
@@ -39,7 +39,10 @@
 	{/if}
 
 	{#if groupedPeople[groupName].length}
-		<h2 class="h2 sm:sticky top-0 bg-white dark:bg-[#0f110f] mb-4">
+		<h2
+			id="{groupName.toLowerCase().replace(/[^a-z]/g, '-')}"
+			class="h2 sm:sticky top-0 bg-white dark:bg-[#0f110f] mb-4"
+		>
 			{groupName === "rest" ? generalGroupName : groupName}
 		</h2>
 		{#each groupedPeople[groupName] as person}
