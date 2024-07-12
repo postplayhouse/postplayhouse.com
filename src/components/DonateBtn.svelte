2e393884eb77b6f85fda78ef3f5fad347cfc7116
Don Denton <don@happycollision.com>
2024-07-12 15:26:18 -0400

    Update donate link

--- src/components/DonateBtn.svelte
+++ src/components/DonateBtn.svelte
@@ -1,6 +1,6 @@
 <div class="flex justify-center items-center my-8">
 	<a
-		href="https://www.eleoonline.net/Pages/WebForms/Mobile/ShowFormMobile.aspx?id=f790c257-b67f-4508-9ff9-0fc3a16f04d2&linkto=670"
+		href="https://www.eleoonline.net/Pages/WebForms/Mobile/ShowFormMobile.aspx?id=c2261d4e-b066-44c5-a26f-fea98d07a3f3&linkto=670"
 		target="_blank"
 		rel="noopener noreferrer"
 		class="btn btn-p inline-block"><slot>Donate Online Now</slot></a
