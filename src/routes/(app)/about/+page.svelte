ff6a65d30fa70585315afc27ddca9b94af7aaecb
Don Denton <don@happycollision.com>
2024-08-26 09:31:42 -0400

    Drop Ken from managing director on about page

--- src/routes/(app)/about/+page.svelte
+++ src/routes/(app)/about/+page.svelte
@@ -35,14 +35,6 @@
 	>
 </p>
 
-<h3 class="h3 mt-4">Managing Director</h3>
-<p>
-	Ken Phillips: <a
-		class="link-green"
-		href="mailto:kenphillips@postplayhouse.com">kenphillips@postplayhouse.com</a
-	>
-</p>
-
 <h3 class="h3 mt-4">Mailing address</h3>
 <p>
 	Post Playhouse<br />
