651affe2eeb3c477b414a2dce486ccd7d1891583
Don Denton <don@happycollision.com>
2025-05-06 21:44:53 -0400

    Copy/paste for 2025 slideshow

---
+++ src/routes/(app)/slideshow/2025/DonorsSmall.svelte
@@ -0,0 +1,110 @@
+<script lang="ts">
+	import { crossfade } from "svelte/transition"
+	import { linear } from "svelte/easing"
+	import { onDestroy } from "svelte"
+	import { browser } from "$app/environment"
+	import { small } from "./donors"
+
+	type Props = {
+		durationMultiplier: number
+		onEventDone: () => void
+	}
+
+	let { durationMultiplier, onEventDone: eventDone }: Props = $props()
+
+	const sections = small
+
+	const slides = $state(sections.map((x) => ({ ...x, position: "bottom" })))
+
+	let current = $state(0)
+
+	function getNextIndex() {
+		return (current + 1) % sections.length
+	}
+
+	let currentTimeout: number
+
+	function nextOrDone() {
+		if (!browser) return
+
+		const nextIndex = getNextIndex()
+		if (nextIndex > 0) {
+			const oldIndex = current
+			current = nextIndex
+			currentTimeout = window.setTimeout(() => {
+				slides[nextIndex]!.position = "top"
+				slides[oldIndex]!.position = "bottom"
+			}, 10)
+		} else {
+			eventDone()
+		}
+	}
+
+	currentTimeout = browser
+		? window.setTimeout(() => (slides[current]!.position = "top"), 10)
+		: 0
+
+	onDestroy(() => clearTimeout(currentTimeout))
+
+	const [send, receive] = crossfade({
+		duration: (distance) => {
+			return distance * durationMultiplier
+		},
+		easing: linear,
+
+		fallback() {
+			return {
+				duration: 0,
+				easing: linear,
+			}
+		},
+	})
+</script>
+
+{#each slides as slide, i}
+	{#if i === current}
+		<article class="absolute inset-0 flex flex-col">
+			<header
+				class="bg-green-700 text-center font-uber text-white [font-size:5vw]"
+			>
+				Annual Fund Donations
+				<div class="bg-green-100 text-black dark:bg-green-900 dark:text-white">
+					{slide.title}
+				</div>
+			</header>
+
+			<div class="relative grow">
+				<div
+					class="absolute inset-0 overflow-hidden text-center font-bold [font-size:5vw]"
+				>
+					<div class="absolute left-0 right-0 [bottom:100vh]">
+						{#if slide.position === "top"}
+							<div
+								in:receive={{ key: slide.title }}
+								out:send={{ key: slide.title }}
+								onintroend={nextOrDone}
+							>
+								{#each slide.names as name}
+									<div>{name}</div>
+								{/each}
+							</div>
+						{/if}
+					</div>
+
+					<div class="absolute left-0 right-0 [top:80vh]">
+						{#if slide.position === "bottom"}
+							<div
+								in:receive={{ key: slide.title }}
+								out:send={{ key: slide.title }}
+							>
+								{#each slide.names as name}
+									<div>{name}</div>
+								{/each}
+							</div>
+						{/if}
+					</div>
+				</div>
+			</div>
+		</article>
+	{/if}
+{/each}
---
+++ src/routes/(app)/slideshow/2025/Slideshow.svelte
@@ -0,0 +1,169 @@
+<script lang="ts">
+	import { fade } from "svelte/transition"
+	import DonorsLarge from "./DonorsLarge.svelte"
+	import DonorsSmall from "./DonorsSmall.svelte"
+	import DonorsSpecial from "./DonorsSpecial.svelte"
+	import { onMount } from "svelte"
+	import {
+		initLocalAppVersion,
+		refreshIfAppVersionOutdated,
+	} from "$helpers/app-version"
+	import { createIntStore } from "$helpers/stores.svelte"
+	import { large, small, special } from "./donors"
+
+	const year = 2025
+
+	onMount(initLocalAppVersion)
+
+	function createPositiveIntStore(initialValue = 1, localStorageName?: string) {
+		return createIntStore(initialValue, {
+			localStorageName,
+			min: 1,
+		})
+	}
+
+	const showsAndTimers = [
+		[
+			DonorsSpecial,
+			createPositiveIntStore(8, `${year} sd`),
+			"Special Donations",
+		],
+		[DonorsLarge, createPositiveIntStore(8, `${year} ld`), "Large Donations"],
+		[
+			DonorsSmall,
+			createPositiveIntStore(10, `${year} rd`),
+			"Regular Donations",
+		],
+	] as const
+
+	const shows = showsAndTimers.map(([show]) => show)
+	const timers = showsAndTimers.map(([_, timer]) => timer)
+	const showNames = showsAndTimers.map(([_, __, name]) => name)
+
+	let inc = $state(0)
+
+	function nextShow() {
+		inc += 1
+
+		const isSixthFullRun = inc % (shows.length * 6) === 0
+
+		if (isSixthFullRun) {
+			refreshIfAppVersionOutdated()
+		}
+	}
+
+	let resetCount = $state(0)
+
+	let currentDurationMultiplier = $derived(
+		timers[inc % timers.length] as (typeof timers)[number],
+	)
+
+	let showInfo = $state(false)
+
+	const info = { special, large, small }
+
+	function onKeyDown(event: KeyboardEvent) {
+		if (event.repeat) return
+
+		switch (event.key) {
+			case "i":
+				event.preventDefault()
+				showInfo = !showInfo
+				break
+			case "ArrowDown":
+				event.preventDefault()
+				currentDurationMultiplier.increment()
+				resetCount = resetCount + 1
+				visible = true
+				break
+			case "ArrowUp":
+				event.preventDefault()
+				currentDurationMultiplier.decrement()
+				resetCount = resetCount + 1
+				visible = true
+				break
+			case "ArrowLeft":
+				event.preventDefault()
+				if (inc === 0) {
+					resetCount = resetCount + 1
+				} else {
+					inc -= 1
+				}
+				break
+			case "ArrowRight":
+				event.preventDefault()
+				inc += 1
+				break
+		}
+	}
+
+	let visible = $state(true)
+
+	$effect(() => {
+		visible = visible && false
+	})
+</script>
+
+<svelte:window onkeydown={onKeyDown} />
+
+{#if showInfo}
+	{#each Object.entries(info) as [section, subsection]}
+		<div class="mt-8 text-xl">
+			<div>
+				<strong>
+					{section.toUpperCase()}
+				</strong>
+			</div>
+		</div>
+		{#each subsection as { title, names }}
+			<div class="mb-6">
+				<div>
+					<strong>
+						{title}
+					</strong>
+				</div>
+
+				{#each names as name}
+					<div>
+						{name}
+					</div>
+				{/each}
+			</div>
+		{/each}
+	{/each}
+{:else}
+	{#key resetCount}
+		<div class="fixed inset-0 dark:bg-black dark:text-white">
+			{#if inc % 2 === 0}
+				{@const ShowComponent = shows[inc % shows.length]}
+				<div transition:fade={{ duration: 1000 }} class="absolute inset-0">
+					<ShowComponent
+						durationMultiplier={currentDurationMultiplier.value}
+						onEventDone={() => nextShow()}
+					/>
+				</div>
+			{:else}
+				{@const ShowComponent = shows[inc % shows.length]}
+				<div transition:fade={{ duration: 1000 }} class="absolute inset-0">
+					<ShowComponent
+						durationMultiplier={currentDurationMultiplier.value}
+						onEventDone={() => nextShow()}
+					/>
+				</div>
+			{/if}
+		</div>
+		{#if visible}
+			<div
+				out:fade={{ duration: 1000, delay: 1000 }}
+				class="fixed inset-0 flex items-center justify-center"
+			>
+				<span
+					class="rounded-lg bg-black/50 px-2 py-1 text-[6vw] font-bold text-white dark:bg-white/50 dark:text-black"
+				>
+					{showNames[inc % showNames.length]} Speed: {20 -
+						(currentDurationMultiplier.value - 1)}
+				</span>
+			</div>
+		{/if}
+	{/key}
+{/if}
