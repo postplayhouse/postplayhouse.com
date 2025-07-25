cab29b9a1e2c2c1937d5789d78e947f1a7d0e5d9
Don Denton <don@happycollision.com>
2025-07-25 13:53:20 -0400

    Add color check

--- src/routes/(app)/debug/+page.svelte
+++ src/routes/(app)/debug/+page.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
 	import OpeningsDebugger from "$components/Openings/OpeningsDebugger.svelte"
+	import ActiveColors from "./ActiveColors.svelte"
 
 	let { data } = $props()
 	const { productions } = data
 </script>
 
+<details>
+	<summary>
+		<h2 class="h2">Active Colors</h2>
+	</summary>
+	<ActiveColors />
+</details>
+
+<h2 class="h2 mt-8">Automatic openings widget</h2>
 <div class="mx-auto mb-16 max-w-3xl p-2">
 	<OpeningsDebugger {productions} />
 </div>
---
+++ src/routes/(app)/debug/ActiveColors.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import { browser } from "$app/environment"
+
+	const rootVars = $derived.by(() => {
+		const rootVars = new Set(browser ? [] : ["green"])
+		const sheets = browser ? Array.from(document.styleSheets) : []
+		for (const sheet of sheets) {
+			try {
+				for (const rule of sheet.cssRules) {
+					const cssText = rule.cssText
+					const matches = [...cssText.matchAll(/--color-([a-z]+)-[0-9]*\s*:/g)]
+					for (const match of matches) {
+						rootVars.add(match[1])
+					}
+				}
+			} catch {
+				continue
+			}
+		}
+		return rootVars
+	})
+</script>
+
+{#each rootVars as name}
+	<div class="grid auto-cols-fr grid-flow-col">
+		{#each ["50", "100", "200", "300", "400", "500", "600", "700", "800", "900", "950"] as value}
+			<div>
+				<div class="unused-bg relative size-12">
+					<div
+						style="background-color: var(--color-{name}-{value});"
+						class="absolute inset-0"
+					></div>
+				</div>
+				<code>bg-{name}-{value}</code>
+			</div>
+		{/each}
+	</div>
+{/each}
+
+<style lang="postcss">
+	@reference "../../../app.css";
+
+	.unused-bg {
+		@apply inset-ring inset-ring-gray-300;
+		background: linear-gradient(
+				45deg,
+				transparent 49%,
+				var(--tw-inset-ring-color) 50%,
+				transparent 51%
+			),
+			linear-gradient(
+				-45deg,
+				transparent 49%,
+				var(--tw-inset-ring-color) 50%,
+				transparent 51%
+			);
+		background-size: 100% 100%;
+		background-repeat: no-repeat;
+	}
+</style>
