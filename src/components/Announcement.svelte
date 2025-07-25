bbcf68370365e7c62ec440e55cb748b50d7fb00f
Don Denton <don@happycollision.com>
2025-07-25 00:29:39 -0400

    Add expiration to Announcement

--- src/components/Announcement.svelte
+++ src/components/Announcement.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { isBefore, startOfToday } from "date-fns"
 	import type { Snippet } from "svelte"
 
 	type Props = {
@@ -6,6 +7,8 @@
 		children: Snippet
 		readMoreText?: string
 		readMoreLink?: string
+		/** YYYY-MM-DD */
+		expirationDate?: `${number}-${number}-${number}`
 	}
 
 	let {
@@ -13,6 +16,7 @@
 		children,
 		readMoreText = "Read more",
 		readMoreLink,
+		expirationDate = "2099-01-01",
 	}: Props = $props()
 </script>
 
@@ -24,28 +28,30 @@
 	{/if}
 {/snippet}
 
-<div
-	class={[
-		"mx-auto mb-16 block max-w-2xl border border-green-400 bg-green-100 p-6 shadow-md dark:border-green-500 dark:bg-green-900/20",
-		readMoreLink &&
-			"group relative isolate block transform-gpu transition-all hover:scale-105 hover:shadow-lg",
-	]}
->
-	<h1 class="h2 mb-2">
-		{#if readMoreLink}
-			<a class="group-hover:underline" href={readMoreLink}>
-				<span class="absolute inset-0 z-10"></span>
+{#if isBefore(startOfToday(), new Date(`${expirationDate}T00:00:00.000`))}
+	<div
+		class={[
+			"mx-auto mb-16 block max-w-2xl border border-green-400 bg-green-100 p-6 shadow-md dark:border-green-500 dark:bg-green-900/20",
+			readMoreLink &&
+				"group relative isolate block transform-gpu transition-all hover:scale-105 hover:shadow-lg",
+		]}
+	>
+		<h1 class="h2 mb-2">
+			{#if readMoreLink}
+				<a class="group-hover:underline" href={readMoreLink}>
+					<span class="absolute inset-0 z-10"></span>
+					{@render renderedTitle()}
+				</a>
+			{:else}
 				{@render renderedTitle()}
-			</a>
-		{:else}
-			{@render renderedTitle()}
-		{/if}
-	</h1>
-	{@render children()}
+			{/if}
+		</h1>
+		{@render children()}
 
-	{#if readMoreLink}
-		<div class="link-green mt-2 text-right">
-			{readMoreText}
-		</div>
-	{/if}
-</div>
+		{#if readMoreLink}
+			<div class="link-green mt-2 text-right">
+				{readMoreText}
+			</div>
+		{/if}
+	</div>
+{/if}
