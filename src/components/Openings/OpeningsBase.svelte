17af88981c9417cbc78eeed58429888861942a68
Don Denton <don@happycollision.com>
2025-09-13 00:07:38 -0500

    Better handling of post-closing in Openings

--- src/components/Openings/OpeningsBase.svelte
+++ src/components/Openings/OpeningsBase.svelte
@@ -88,110 +88,109 @@
 	<p class="my-8 text-3xl">
 		We were delighted to bring you a summer season full of entertainment!
 	</p>
-{:else}
-	{#if openingSoon}
-		<h3 class="h1 font-uber">
-			{openingSoon.title} opens {openingSoon.daysUntilOpening > 0
-				? `on ${openingSoon.dayOfWeek}`
-				: "today"}!
-		</h3>
+{/if}
+{#if openingSoon}
+	<h3 class="h1 font-uber">
+		{openingSoon.title} opens {openingSoon.daysUntilOpening > 0
+			? `on ${openingSoon.dayOfWeek}`
+			: "today"}!
+	</h3>
 
-		<div class="my-8 items-center md:flex">
-			<div class="shrink">
-				<DynamicCurrentSeasonImage
-					imageFile={openingSoon.image}
-					alt="Show Logo for {openingSoon.title}"
-				/>
-			</div>
-			<div class="shrink-0 text-center md:text-left">
-				<Markdown source={openingSoon.writers} />
-			</div>
+	<div class="my-8 items-center md:flex">
+		<div class="shrink">
+			<DynamicCurrentSeasonImage
+				imageFile={openingSoon.image}
+				alt="Show Logo for {openingSoon.title}"
+			/>
 		</div>
-
-		<Markdown source={openingSoon.description} />
-
-		<div class="m-4 flex justify-center">
-			<TicketsButton />
+		<div class="shrink-0 text-center md:text-left">
+			<Markdown source={openingSoon.writers} />
 		</div>
-	{/if}
+	</div>
 
-	{#if daysToClosing < 30}
-		<div class="my-12">
-			<h3 class="h1 mb-4">Hurry, time is running out!</h3>
-			{#if daysToClosing > 1}
-				<p class="text-2xl">
-					There are only <span class="text-3xl font-bold"
-						>{daysToClosing} more days</span
-					>
-					until our final performance on {formatDate(closingDate, {
-						skipYear: true,
-					})}!
-				</p>
-			{:else if daysToClosing === 1}
-				<p class="h3 text-center">Tomorrow is our final performance!</p>
-			{:else}
-				<p class="h3 text-center">
-					Today is your last chance to see us this summer!
-				</p>
-			{/if}
-			{@render ticketAvailability?.()}
-		</div>
-	{/if}
+	<Markdown source={openingSoon.description} />
 
-	{#if enhancedProductions.length > 0}
-		<h3 class="h1 my-8">
-			Our {nonValueToEmptyStr(enhancedProductions.at(0)?.season)} Summer Season
-		</h3>
+	<div class="m-4 flex justify-center">
+		<TicketsButton />
+	</div>
+{/if}
 
-		<div class="mb-12">
-			<SponsorPlate>
-				{#snippet beforeSponsors()}
-					<h3 class="h3 mb-4 text-center">
-						Special thanks to our Season Sponsors:
-					</h3>
-				{/snippet}
-			</SponsorPlate>
-		</div>
-
-		{#if allShowsAreRunning}
-			<div class="my-8 text-center text-3xl">All shows are now running!</div>
+{#if daysToClosing < 30 && isBeforeClosing}
+	<div class="my-12">
+		<h3 class="h1 mb-4">Hurry, time is running out!</h3>
+		{#if daysToClosing > 1}
+			<p class="text-2xl">
+				There are only <span class="text-3xl font-bold"
+					>{daysToClosing} more days</span
+				>
+				until our final performance on {formatDate(closingDate, {
+					skipYear: true,
+				})}!
+			</p>
+		{:else if daysToClosing === 1}
+			<p class="h3 text-center">Tomorrow is our final performance!</p>
+		{:else if daysToClosing === 0}
+			<p class="h3 text-center">
+				Today is your last chance to see us this summer!
+			</p>
 		{/if}
+		{@render ticketAvailability?.()}
+	</div>
+{/if}
 
-		{@render seasonArtworkImage?.()}
+{#if enhancedProductions.length > 0}
+	<h3 class="h1 my-8">
+		Our {nonValueToEmptyStr(enhancedProductions.at(0)?.season)} Summer Season
+	</h3>
 
+	<div class="mb-12">
+		<SponsorPlate>
+			{#snippet beforeSponsors()}
+				<h3 class="h3 mb-4 text-center">
+					Special thanks to our Season Sponsors:
+				</h3>
+			{/snippet}
+		</SponsorPlate>
+	</div>
+
+	{#if allShowsAreRunning}
+		<div class="my-8 text-center text-3xl">All shows are now running!</div>
+	{/if}
+
+	{@render seasonArtworkImage?.()}
+
+	{#if isBeforeClosing}
 		<div class="my-12 flex justify-center">
 			<TicketsButton />
 		</div>
+	{/if}
 
-		{#if !allShowsAreRunning && nowRunning.length > 0}
-			<div>
-				<div
-					class="mb-8 flex flex-wrap justify-center gap-12 *:w-fit *:shrink-0"
-				>
-					<div>
-						<h4 class="h2">Now running:</h4>
-						<ul class="list-none pl-2">
-							{#each nowRunning as production}
-								<li class="">
-									✅ {production.title}
-								</li>
-							{/each}
-						</ul>
-					</div>
-					<div>
-						<h4 class="h2">Opening Soon:</h4>
-						<ul class="list-none pl-2">
-							{#each notYetOpen as production}
-								<li class="">
-									⏳ {production.title} - {formatDate(production.opening, {
-										skipYear: true,
-									})}
-								</li>
-							{/each}
-						</ul>
-					</div>
+	{#if !allShowsAreRunning && nowRunning.length > 0 && isBeforeClosing}
+		<div>
+			<div class="mb-8 flex flex-wrap justify-center gap-12 *:w-fit *:shrink-0">
+				<div>
+					<h4 class="h2">Now running:</h4>
+					<ul class="list-none pl-2">
+						{#each nowRunning as production}
+							<li class="">
+								✅ {production.title}
+							</li>
+						{/each}
+					</ul>
+				</div>
+				<div>
+					<h4 class="h2">Opening Soon:</h4>
+					<ul class="list-none pl-2">
+						{#each notYetOpen as production}
+							<li class="">
+								⏳ {production.title} - {formatDate(production.opening, {
+									skipYear: true,
+								})}
+							</li>
+						{/each}
+					</ul>
 				</div>
 			</div>
-		{/if}
+		</div>
 	{/if}
 {/if}
