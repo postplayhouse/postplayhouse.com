b3ec3a6dccf1ee94f15d8d2dc07f4a1ed65ef87b
Don Denton <don@happycollision.com>
2025-11-13 18:31:44 -0600

    Refactor: generic opening images

--- src/components/Openings/Openings.svelte
+++ src/components/Openings/Openings.svelte
@@ -10,6 +10,7 @@
 	}
 
 	let { productions = [], season, debugTodayString }: Props = $props()
+	let rotations = ["-rotate-2", "rotate-3", "rotate-2", "-rotate-2", "rotate-2"]
 </script>
 
 <OpeningBase {season} {productions} {debugTodayString}>
@@ -17,56 +18,20 @@
 		<ul
 			class="-m-4 flex list-none flex-wrap items-center justify-center gap-2 bg-green-200 p-6 *:max-w-96 sm:rounded-sm sm:*:max-w-60 dark:bg-transparent"
 		>
-			<li
-				class="block -rotate-2 overflow-clip rounded-[10%] shadow-lg shadow-green-800/50 md:scale-105 dark:scale-100 dark:bg-gray-300 dark:shadow-black/20"
-			>
-				<SeasonImage
-					class="max-w-full mix-blend-multiply"
-					imageFile="guys-n-dolls.jpg"
-					season="2025"
-					alt="Show Logo for guys-n-dolls"
-				></SeasonImage>
-			</li>
-			<li
-				class="block rotate-3 overflow-clip rounded-[10%] shadow-lg shadow-green-800/50 md:scale-105 dark:scale-100 dark:bg-gray-300 dark:shadow-black/20"
-			>
-				<SeasonImage
-					class="max-w-full mix-blend-multiply"
-					imageFile="nunsense-jamboree.jpg"
-					season="2025"
-					alt="Show Logo for nunsense-jamboree"
-				></SeasonImage>
-			</li>
-			<li
-				class="md:rotate dark:scale-100-0 block rotate-2 overflow-clip rounded-[10%] shadow-lg shadow-green-800/50 md:scale-105 dark:bg-gray-300 dark:shadow-black/20"
-			>
-				<SeasonImage
-					class="max-w-full mix-blend-multiply"
-					imageFile="newsies.jpg"
-					season="2025"
-					alt="Show Logo for newsies"
-				></SeasonImage>
-			</li>
-			<li
-				class="block -rotate-2 overflow-clip rounded-[10%] shadow-lg shadow-green-800/50 md:scale-105 dark:scale-100 dark:bg-gray-300 dark:shadow-black/20"
-			>
-				<SeasonImage
-					class="max-w-full mix-blend-multiply"
-					imageFile="lucky-stiff.jpg"
-					season="2025"
-					alt="Show Logo for lucky-stiff"
-				></SeasonImage>
-			</li>
-			<li
-				class="block rotate-2 overflow-clip rounded-[10%] shadow-lg shadow-green-800/50 md:scale-105 dark:scale-100 dark:bg-gray-300 dark:shadow-black/20"
-			>
-				<SeasonImage
-					class="max-w-full mix-blend-multiply"
-					imageFile="into-the-woods.jpg"
-					season="2025"
-					alt="Show Logo for into-the-woods"
-				></SeasonImage>
-			</li>
+			{#each productions as production, index}
+				<li
+					class="block overflow-clip rounded-[10%] shadow-lg shadow-green-800/50 md:scale-105 dark:scale-100 dark:bg-gray-300 dark:shadow-black/20 {rotations[
+						index % rotations.length
+					]}"
+				>
+					<SeasonImage
+						class="max-w-full bg-white p-2 mix-blend-multiply"
+						imageFile={production.image}
+						{season}
+						alt="Show Logo for {production.title}"
+					></SeasonImage>
+				</li>
+			{/each}
 		</ul>
 	{/snippet}
 </OpeningBase>
