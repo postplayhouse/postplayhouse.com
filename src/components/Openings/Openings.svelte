aa6b282d27a8b52e0369cdf06e1f141a2e4d8f3c
Don Denton <don@happycollision.com>
2025-12-11 23:56:40 -0600

    Refactor/Fix assertion on only first reactive value
    The original assertion was only on the very first value that the props could have. Updated values would never be re-evaluated. This made TS happy, but was not correct.

--- src/components/Openings/Openings.svelte
+++ src/components/Openings/Openings.svelte
@@ -1,3 +1,24 @@
+<script lang="ts" module>
+	import z from "zod"
+
+	const guaranteedOpeningSchema = z.array(
+		z.looseObject({
+			opening: z.iso.date(),
+		}),
+	)
+
+	function assertProdsHaveOpenings<P>(
+		productions: P[],
+	): asserts productions is (P & { opening: string })[] {
+		guaranteedOpeningSchema.parse(productions)
+	}
+
+	function assertedProdsHaveOpenings<P extends Production>(productions: P[]) {
+		assertProdsHaveOpenings(productions)
+		return productions
+	}
+</script>
+
 <script lang="ts">
 	import SeasonImage from "$components/SeasonImage.svelte"
 	import OpeningBase from "$components/Openings/OpeningsBase.svelte"
@@ -13,7 +34,11 @@
 	let rotations = ["-rotate-2", "rotate-3", "rotate-2", "-rotate-2", "rotate-2"]
 </script>
 
-<OpeningBase {season} {productions} {debugTodayString}>
+<OpeningBase
+	{season}
+	productions={assertedProdsHaveOpenings(productions)}
+	{debugTodayString}
+>
 	{#snippet seasonArtworkImage()}
 		<ul
 			class="-m-4 flex list-none flex-wrap items-center justify-center gap-2 bg-green-200 p-6 *:max-w-96 sm:rounded-sm sm:*:max-w-60 dark:bg-transparent"
