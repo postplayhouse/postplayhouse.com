e0a6e7b972e3c85e0d6fabf1465f28dc94a15fa4
Don Denton <don@happycollision.com>
2025-12-15 01:07:55 -0500

    Extract data/seasons from data/site
    This needs to be separate so it can be safely imported into svelte.config.js

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,5 +1,7 @@
 import { dev } from "$app/environment"
 
+export { season, allYears } from "./seasons"
+
 const prod = !dev
 
 // Actual values. If dev, these may change below.
@@ -9,7 +11,6 @@ const showsAnnounced_ = true as boolean
 export const ticketsAvailableDate = "2025-11-15T00:00:00"
 export const url = "__PUBLIC_BUILD_URL__" // replaced by vite.config.ts
 export const castingComplete = prod ? castingComplete_ : true
-export const season = 2026 satisfies Date.Year
 export const description =
 	"Post Playhouse, Inc. is Northwestern Nebraska's favorite live theatre company, producing several musicals running in a repertory schedule every summer by bringing together highly skilled creative professionals from across the country and nearby. Post Playhouse is located in historic Fort Robinson State Park."
 
@@ -20,14 +21,4 @@ export const ticketsLink = "https://postplayhouse.showare.com/"
 
 export function ticketsAvailable() {
 	return new Date() >= new Date(ticketsAvailableDate)
-}
-
-export const allYears = [
-	2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026,
-] as const
-
-if (!allYears.includes(season)) {
-	throw new Error(
-		"Don't forget to add new seasons here so they appear after the next season rolls over",
-	)
 }
