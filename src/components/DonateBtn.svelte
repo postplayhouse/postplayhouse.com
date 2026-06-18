759d743585da24841a26eff4512eeb912f75e5b0
Don Denton <don@happycollision.com>
2026-06-18 00:30:46 -0400

    New online donation location

--- src/components/DonateBtn.svelte
+++ src/components/DonateBtn.svelte
@@ -6,7 +6,7 @@
 
 <div class="my-8 flex items-center justify-center">
 	<a
-		href="https://www.eleoonline.net/Pages/WebForms/Mobile/ShowFormMobile.aspx?id=c2261d4e-b066-44c5-a26f-fea98d07a3f3&linkto=670"
+		href="https://webforms.eleoonline.com/FormDatas/350/7f924e41-8828-4325-88d2-aa5b49c31b43"
 		target="_blank"
 		rel="noopener noreferrer"
 		class="btn btn-p inline-block"
