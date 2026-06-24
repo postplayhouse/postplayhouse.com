551998f7e1846bcbe24474beb0fdd7c6cacd954d
Don Denton <don@happycollision.com>
2026-06-24 14:09:28 -0400

    Reword wildfire message

--- src/components/WildfireRecovery/WildfireRecovery.svelte
+++ src/components/WildfireRecovery/WildfireRecovery.svelte
@@ -21,13 +21,15 @@
 
 	<div class="space-y-3">
 		<p>
-			The week of June 8, 2026, wildfire temporarily disrupted our season at
-			Fort Robinson State Park.
+			We recently shared how the South Fork Fire disrupted the start of our
+			season at Post Playhouse. Today, we’re grateful to share part of our
+			Comeback Story.
 		</p>
 		<p>
-			Our entire company was safely evacuated, and thanks to firefighters and
-			emergency crews, the historic fort and surrounding area were protected. We
-			are deeply grateful.
+			Thanks to the generosity of our incredible supporters near and far, nearly
+			$5,000 has already been raised toward our $45,000 Wildfire Recovery Fund
+			goal. Every gift has had an impact on our artistry and work, our spirits,
+			and our communities here in Northwest Nebraska.
 		</p>
 	</div>
 
@@ -35,29 +37,24 @@
 	<div>
 		<div class="space-y-3 pt-3">
 			<p>
-				While Fort Robinson has reopened, the start of our season was
-				significantly impacted—three performances were cancelled, emergency
-				expenses were incurred, and attendance has been affected during a
-				critical time for our nonprofit theatre.
+				Like so many businesses, attractions, and organizations across Northwest
+				Nebraska, Post Playhouse depends on a strong summer season. For nearly
+				60 years, we have brought people together and contributed to the summer
+				experience at Fort Robinson State Park– part of what makes this region
+				so special.
 			</p>
 			<p>
-				Post Playhouse depends on ticket sales and community support to bring
-				professional theatre to the Nebraska Panhandle each summer.
+				While the emergency phase of the South Fork Fire has passed, the impact
+				of 3 cancelled performances, emergency and evacuation expenses, and lost
+				attendance during this important moment in our season, is still deeply
+				felt at our nonprofit theatre.
 			</p>
 			<p>
-				We are launching a Wildfire Recovery Fund with a goal of $45,000 to help
-				us recover and sustain the remainder of the season.
-			</p>
-			<p>
-				Your gift will cover emergency costs, help support our artists and
-				staff, and keep professional theatre thriving at Fort Robinson.
-			</p>
-			<p>
-				If Post Playhouse has ever been part of your Fort Robinson story, we
-				hope you’ll consider making a gift today.
-			</p>
-			<p class="font-medium">
-				Wildfire may have interrupted our season, but it did not end it.
+				Our recovery continues as we write Post Playhouse’s next chapter of
+				Northwest Nebraska’s story. If Post Playhouse has ever been part of your
+				story, or if you simply want to support the recovery of a region that
+				relies on summer tourism, we invite you to play a critical role in our
+				Comeback Story and make a gift today.
 			</p>
 		</div>
 
